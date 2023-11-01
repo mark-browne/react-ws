2ddfb5494388df04e48c71d97853833e0ee0002d
mark browne <markbrowne789@hotmail.com>
2023-11-01 17:44:50 +0000

    connect to ws

--- src/app.js
+++ src/app.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from "react";
 import './app.css';
 
+function  App() {
+  //give an initial state so that the data won't be undefined at start
+  const [bids, setBids] = useState([0]);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
-    </div>
-  );
+  const ws = new WebSocket("wss://ws.bitstamp.net");
+
+  const apiCall = {
+    event: "bts:subscribe",
+    data: { channel: "order_book_btcusd" },
+  };
+
+  ws.onopen = (event) => {
+    ws.send(JSON.stringify(apiCall));
+  };
+
+  ws.onmessage = function (event) {
+    const json = JSON.parse(event.data);
+    try {
+      if ((json.event = "data")) {
+        setBids(json.data.bids.slice(0, 5));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  //map the first 5 bids
+  const firstBids = bids.map((item) => {
+    return (
+      <div>
+        <p> {item}</p>
+      </div>
+    );
+  });
+
+  return <div>{firstBids}</div>;
 }
 
-export default App;
+export default  App;
